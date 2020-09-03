c2781a68b23317e02d6200987af6ad28ae3369ee
Ethan Harris <ethan.s.harris@protonmail.com>
2020-09-03 17:23:35 -0400

    Uploading login.php and register.php api

---
+++ api/Register.php
@@ -0,0 +1,45 @@
+<?php
+	$inData = getRequestInfo();
+	$servername = "localhost";
+	$database = "eyeContacts";
+	$username = "creator";
+	$password = "plsdonthackmebro2";
+	
+	$username = $inData["username"];
+	$password = $inData["password"];
+
+	$conn = new mysqli($servername, $username, $password, $database);
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "insert into login_info (username,password) VALUES (" . $username . ",'" . $password . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
