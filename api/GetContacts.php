e29669f873c9b9eaaf31a24573d7dd1fa664dd46
Ethan Harris <ethan.s.harris@protonmail.com>
2020-09-09 13:35:10 -0400

    Uploading GetContacts api

---
+++ api/GetContacts.php
@@ -0,0 +1,67 @@
+<?php
+	$inData = getRequestInfo();
+	$servername = "localhost";
+	$database = "eyeContacts";
+	$username = "creator";
+	$password = "plsdonthackmebro2";
+	
+	$unameID = $inData["unameID"];;
+	$contactResults = '{"contacts":[';
+	$numContacts = 0;
+	
+	$conn = new mysqli($servername, $username, $password, $database);
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "SELECT * FROM contacts where unameID='" . $inputUname . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			while($row = $result->fetch_assoc())
+			{
+				if( $numContacts > 0 )
+				{
+					$searchResults .= ",";
+				}
+				$contactResults .= '"contactsNumber":"' . $row["contactsNumber"] .'","contactFirstName":"' .
+				$row["contactFirstName"] .'","contactLastName":"' . $row["contactLastName"] . '","email":"' .
+				$row["email"] .'","phoneNumber":"' . $row["phoneNumber"] '","dateCreated":"' . $row["dateCreated"]
+				. '"]';
+				$numContacts++;
+			}			
+			returnWithInfo($contactResults);
+		}
+		else
+		{
+			returnWithError( "No Contacts Found" );
+		}
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"contacts":"[[]]","numContacts":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $unameID )
+	{
+		$retValue = $contactResults . '"numContacts":' . numContacts . ',"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
