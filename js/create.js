2107be071bbd638cddf51aaf71b4d96230ff404b
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-09-11 10:15:06 -0400

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,19 +1,23 @@
 $("#ajaxform").submit(function(e){
 $(document).ready(function(){
     $("#createUserButton").on("click",function(){
+        username: $("#Username").val(),
+        password: $("#Password").val(),
+        verifiedPassword: $("#verify").val(),
+        if(password != verifiedPassword)
+            alert("Passwords don't match");
         $.ajax({
             type: "POST",
             url: "Register.php",
             data: {
-                username: $("#Username").val(),
-                password: $("#Password").val(),
-                verifiedPassword: $("#verify").val(),
+                username: username,
+                password: password,
+                verifiedPassword: verifiedPassword,
                 success: function(data){
-                    if(password != verifiedPassword)
-                        alert("Passwords don't match");
+          
                     else
                     {
-                        if(data == "error")
+                        if(data.error == "")
                            alert("Username already used");
                         else
                             alert("Account created");
