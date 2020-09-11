77945c1b270c37ffcae69db2f863e7dba8394b3a
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-09-11 10:55:42 -0400

    Update create.js

--- create.js
+++ create.js
@@ -1,31 +1,36 @@
-$("#ajaxform").submit(function(e){
-$(document).ready(function(){
-    $("#createUserButton").on("click",function(){
-        username: $("#Username").val(),
-        password: $("#Password").val(),
-        verifiedPassword: $("#verify").val(),
-        if(password != verifiedPassword)
-            alert("Passwords don't match");
-        $.ajax({
-            type: "POST",
-            url: "Register.php",
-            data: {
-                username: username,
-                password: password,
-                verifiedPassword: verifiedPassword,
-                success: function(data){
-          
-                    else
-                    {
-                        if(data.error == "")
-                           alert("Username already used");
-                        else
-                            alert("Account created");
-                    }
-                       
-                },
-            },
-        });
-      });
-    });
-  });
+function create(){
+	var url = 'http://plsgiveusana.me/api/Register.php';
+	var username = document.getElementById("Username").value;
+	var password = document.getElementById("Password").value;
+	var verifiedPassword = document.getElementById("verify").value;
+	if(username == "" || password == "" || verifiedPassword == "")
+	{
+		alert("Enter all required fields");
+		return;
+	}
+	if(password != verifiedPassword)
+	{
+		alert("Passwords don't match");
+		return;
+	}
+	var data= '{"username" : "' + username +'", "password" : "' + password + '"}';
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST",url,false);
+    xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+    try
+    {
+        xhr.send(data);
+        var response = JSON.parse(xhr.responseText); 
+        var error = response["error"];
+        if(error == "")
+        {
+        	alert("User created");
+        	return;
+        }
+        else
+        	alert("Error");
+    }
+    catch(error){
+    	alert(error.message);
+    }
+}
