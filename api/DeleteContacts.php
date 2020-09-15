b5a29c84b466bfe53f5dd4cace09c41203d0026d
Ethan Harris <ethan.s.harris@protonmail.com>
2020-09-15 11:06:51 -0400

    fixd

--- api/DeleteContacts.php
+++ api/DeleteContacts.php
@@ -14,7 +14,7 @@
 	} 
 	else
 	{
-		$sql = "DELETE FROM contacts where contactNumber='" . $contactNumber;
+		$sql = "DELETE FROM contacts where contactNumber=" . $contactNumber;
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
