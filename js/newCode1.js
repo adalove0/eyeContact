bd672ef2d7d62125395d88a0e05f7e6de7e259e7
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-09-23 14:18:46 -0400

    Update newCode1.js

--- js/newCode1.js
+++ js/newCode1.js
@@ -416,7 +416,7 @@ function searchContact()
 			          
 			  else
 				  alert("NO RECORDS");
-			   alert("OBJECT: "+jsonObject);
+			   alert("OBJECT: "+JSON.stringify(jsonObject));
 					
 		 } 
 		 else
