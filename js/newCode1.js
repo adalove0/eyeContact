9f3f98ec41c264efbf00993baa5e173d2f67c0fb
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-09-23 13:19:28 -0400

    Update newCode1.js

--- js/newCode1.js
+++ js/newCode1.js
@@ -177,7 +177,7 @@ function read()
 
 function displayContacts(jsonData)
 {
-    table = document.createElement("tbody");
+    var table = document.createElement("tbody");
     table.id = "display-table";
     document.getElementById("displayTable").appendChild(table);
     var num = jsonData["numContacts"];
@@ -422,7 +422,7 @@ function searchContact()
 			  else
 				  alert("NO RECORDS");
 			   alert(jsonObject);
-			   window.location.href = "test-page.html";
+			   displaySearch(jsonObject);
 					
 		 } 
 		 else
@@ -438,4 +438,174 @@ function searchContact()
      }
 					
 					
+}
+function displaySearch(jsonData)
+{
+    var oldTable = document.getElementById("display-table");
+    oldTable.remove();
+    var table = document.createElement("tbody");
+    table.id = "display-table";
+    document.getElementById("display-search-table").appendChild(table);
+    var buttonBack = document.createElement("button");
+    buttonBack.innerHTML = "BACK";
+    buttonBack.addEventListener("click", function() {
+      window.location.href = "test-page.html";
+    });
+    var num = jsonData["numContacts"];
+    var contactsList = jsonData["contacts"];
+    var d2, d1, d3;
+    for(var i = 0 ; i < num; i++)
+    {
+         var response = contactsList[i];
+         var row = table.insertRow();
+         var contactnumero = i;
+         var cellName = row.insertCell();
+         cellName.classList.add("accordion");
+         cellName.style.cursor = "pointer";
+         cellName.id = response.contactNumber;
+         cellName.style.width = "780px";
+         cellName.innerHTML = response.contactFirstName+" "+ response.contactLastName;
+        var cellDelete = row.insertCell();
+        cellDelete.id = response.contactNumber;
+        cellDelete.style.cursor = "pointer";
+        cellDelete.innerHTML = '<i style="font-size:24px" class="fa">&#xf014;</i>';
+        cellDelete.addEventListener("click", function() {
+            deleteContact(this.id);
+         });
+        var contentDiv1 = document.createElement("div");
+        var newLine = document.createElement("br");
+        contentDiv1.id = "contact" + response.contactNumber;
+        var image = document.createElement("img");
+        image.src= "images/user.jpg";
+        image.id = "user-img";
+        contentDiv1.style.display = "none";
+        var infotable = document.createElement("table");
+        var fnamerow = infotable.insertRow();
+        var lnamerow = infotable.insertRow();
+        var emailrow = infotable.insertRow();
+        var phonerow = infotable.insertRow();
+        var fname = document.createElement("p");
+        var lname = document.createElement("p");
+        var emailPhoneP = document.createElement("p");
+        var phone = document.createElement("p");
+        fname.id = "show-fname"+response.contactNumber;
+        emailPhoneP.id = "show-email"+response.contactNumber;
+        phone.id = "show-phone"+response.contactNumber;
+        lname.id = "show-lname"+response.contactNumber;
+        fname.innerHTML = "First name: "+'&nbsp &nbsp &nbsp &nbsp' + response.contactFirstName;
+        fnamerow.appendChild(fname);
+        lname.innerHTML = "Last name: "+'&nbsp &nbsp &nbsp &nbsp &nbsp' + response.contactLastName;
+        lnamerow.appendChild(lname);
+        emailPhoneP.innerHTML = "Email: "+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + response.email;
+        emailrow.appendChild(emailPhoneP);
+        phone.innerHTML = "Phone "+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + response.phoneNumber;
+        phonerow.appendChild(phone);
+        contentDiv1.appendChild(image);
+        contentDiv1.appendChild(infotable);
+        var editButton = document.createElement("button");
+        editButton.innerHTML = '<i class="material-icons" style="font-size:36px">mode_edit</i>';
+        editButton.id = response.contactNumber;
+        contentDiv1.appendChild(editButton);
+        var backButton1 = document.createElement("button");
+        backButton1.innerHTML = "Button2";
+        contentDiv1.appendChild(backButton1);
+        var contentDiv2 = document.createElement("div");
+        contentDiv2.id = "form" + response.contactNumber;
+        contentDiv2.appendChild(image);
+        var addForm = document.createElement("form");
+        var fnamelabel = document.createElement("label");
+        fnamelabel.setAttribute("for","fnamelabel");
+        fnamelabel.htmlFor = "text";
+        fnamelabel.innerHTML = "First name: ";
+        var fnameinput = document.createElement("input");
+        fnameinput.id = "editFname"+response.contactNumber;
+        fnameinput.value = response.contactFirstName;
+        addForm.appendChild(fnamelabel);
+        addForm.appendChild(newLine);
+        addForm.appendChild(fnameinput);
+        addForm.appendChild(newLine.cloneNode());
+        var lnamelabel = document.createElement("label");
+        lnamelabel.setAttribute("for","lnamelabel");
+        lnamelabel.htmlFor = "text";
+        lnamelabel.innerHTML = "Last name: ";
+        var lnameinput = document.createElement("input");
+        lnameinput.value = response.contactLastName;
+        lnameinput.id = "editLname"+response.contactNumber;
+        addForm.appendChild(lnamelabel);
+        addForm.appendChild(newLine.cloneNode());
+        addForm.appendChild(lnameinput);
+        addForm.appendChild(newLine.cloneNode());
+        var phonelabel = document.createElement("label");
+        phonelabel.setAttribute("for","phonelabel");
+        phonelabel.htmlFor = "text";
+        phonelabel.innerHTML = "Phone: ";
+        var phoneinput = document.createElement("input");
+        phoneinput.id = "editPhone"+response.contactNumber;
+        phoneinput.value = response.phoneNumber;
+        addForm.appendChild(phonelabel);
+        addForm.appendChild(newLine.cloneNode());
+        addForm.appendChild(phoneinput);
+        addForm.appendChild(newLine.cloneNode());
+        var emailabel = document.createElement("label");
+        emailabel.setAttribute("for","emailabel");
+        emailabel.htmlFor = "text";
+        emailabel.innerHTML = "Email: ";
+        var emailinput = document.createElement("input");
+        emailinput.id = "editEmail"+response.contactNumber;
+        emailinput.value = response.email;
+        addForm.appendChild(emailabel);
+        addForm.appendChild(newLine.cloneNode());
+        addForm.appendChild(emailinput);
+        addForm.appendChild(newLine.cloneNode());
+        contentDiv2.appendChild(addForm);
+        contentDiv1.classList.add("new-form");
+        var backButton = document.createElement("button");
+        backButton.innerHTML = "Button1";
+        var doneButton = document.createElement("button");
+        doneButton.innerHTML = "DONE";
+        doneButton.id = response.contactNumber;;
+        doneButton.addEventListener("click", function(){
+         alert("Yes");
+         var editID = "form" + this.id;
+          d2 = document.getElementById("contact"+this.id);
+          d3  = document.getElementById(editID);
+           edit(this.id);
+           d2.style.display = "block";
+           d3.style.display = "none";
+        });
+        contentDiv2.appendChild(backButton);
+        contentDiv2.appendChild(doneButton);
+        contentDiv2.style.display = "none";
+        document.getElementById("edit-container").appendChild(contentDiv2);
+        document.getElementById("big-container").appendChild(contentDiv1);
+        editButton.addEventListener("click", function(){
+            alert("Hello");
+            var editID = "form" + this.id;
+            d2 = document.getElementById("contact"+this.id);
+            d3  = document.getElementById(editID);
+            if(d3.style.display == "none")
+            {
+                d2.style.display = "none";
+                d3.style.display = "block";
+            }
+        });
+
+        cellName.addEventListener("click", function(){
+            var cellID = "contact"+this.id;
+            d1 = document.getElementById("contactView");
+            d2 = document.getElementById(cellID);
+            if(d2.style.display == "none")
+            {
+                d1.style.display = "none";
+                d2.style.display = "block";
+             }
+        });
+        backButton.addEventListener("click",function(){
+                d3.style.display = "none";
+                d2.style.display = "block";
+        });
+        backButton1.addEventListener("click",function(){
+           d2.style.display = "none";
+            d1.style.display = "block";
+        });
 }
