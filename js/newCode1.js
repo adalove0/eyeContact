926d501f451253c7a29884edba63df5cedd95353
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-09-23 09:18:44 -0400

    Update newCode1.js

--- js/newCode1.js
+++ js/newCode1.js
@@ -117,6 +117,7 @@ var userID = 0;
         var email = document.getElementById("email").value;
         readCookie();
         var data = '{"unameID" : "' + userID + '", "contactFirstName" : "' + ContactFirstName+ '", "contactLastName" : "' + ContactLastName +'", "email" : "'+email+'", "phoneNumber" : "' +phone + '"}';
+        alert(data);
         let xhr = new XMLHttpRequest();
         xhr.open("POST",url,true);
         xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
